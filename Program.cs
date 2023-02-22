b4982670a507ff1684b4597871fa7f329d75211e
PrimAIII <126025564+PrimAIII@users.noreply.github.com>
2023-02-22 17:31:27 +0000

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("The current time is " + DateTime.Now);
+string MyWorld = "My World is a Marvel";
+Console.WriteLine(MyWorld);
+MyWorld = MyWorld.Replace("World", "Life");
+Console.WriteLine(MyWorld);
